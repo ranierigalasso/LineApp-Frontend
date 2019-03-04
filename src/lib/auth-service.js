eb742cf24a8cbc8fae8c9059a63a97ecb444de70
ranierigalasso <ranierigalasso@gmail.com>
2019-03-04 17:56:14 +0100

    deploy again

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { eventNames } from 'cluster';
 
 class Auth {
   constructor() {
--- src/lib/profile-service.js
+++ src/lib/profile-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class Profile {
   constructor() {
     this.profile = axios.create({
-      baseURL: 'http://localhost:5000',
+      baseURL: process.env.REACT_APP_BASE_URL,
       withCredentials: true
     })
   }
