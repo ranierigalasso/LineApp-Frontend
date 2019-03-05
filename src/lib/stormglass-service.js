df291db314962f6be72ab4421f059f334e38f3ea
ranierigalasso <ranierigalasso@gmail.com>
2019-03-05 14:17:52 +0100

    done forecast on post

---
+++ src/lib/stormglass-service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+class Forecast {
+  getForecast(lat, long, params) {
+    return fetch(`https://api.stormglass.io/point?lat=${lat}&lng=${long}&params=${params}`, {
+      headers: {
+      'Authorization': `${process.env.REACT_APP_STORMGLASS}`
+      }
+    })
+    .then((response) => {
+
+      return response.json()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+}
+
+const ForecastService = new Forecast();
+
+export default ForecastService;
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import { withAuth } from '../components/AuthProvider';
 import PostService from '../lib/post-service';
-import { Form, Button } from 'react-bootstrap';
+import ForecastService from '../lib/stormglass-service';
+import { Form, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Comment from '../components/Comment';
 import Like from '../components/Like';
+
 import '../stylesheets/Post.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinusCircle, faEdit, faCommentDots } from '@fortawesome/free-solid-svg-icons'
+import { faMinusCircle, faEdit, faCommentDots, faWater } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faMinusCircle, faEdit, faCommentDots);
+library.add(faMinusCircle, faEdit, faCommentDots, faWater);
 
 class Post extends Component {
   state = {
@@ -25,6 +27,11 @@ class Post extends Component {
     comment: '',
     comments: [],
     likes: 0,
+    forecastToggle: false,
+    lat: 0,
+    long: 0,
+    forecastData: null,
+    loading: true,
   }
   componentDidMount = () => {
     this.getPost();
@@ -35,6 +42,8 @@ class Post extends Component {
     PostService.getPost(id)
       .then((data) => {
         const {createdAt, description, imageUrl, location} = data;
+        const long = data.coords.coordinates[0];
+        const lat = data.coords.coordinates[1];
         const creatorId = data.creatorId._id;
         const username = data.creatorId.username;
         const profileImg = data.creatorId.profileImg;
@@ -46,6 +55,8 @@ class Post extends Component {
           location,
           username,
           profileImg,
+          lat,
+          long,
         })
       })
       .catch((error) => {
@@ -75,17 +86,11 @@ class Post extends Component {
             <button className='universal-button' variant="primary" type="submit">
               <FontAwesomeIcon icon="minus-circle" size="1x" />              
             </button>
-            {/* <Button variant="primary" type="submit">
-              <FontAwesomeIcon icon="minus-circle" size="1x" />              
-            </Button> */}
           </Form>
           <Link to={`/post/${postId}/edit`}>
-          <button className='universal-button'variant="primary" type="submit">
+            <button className='universal-button'variant="primary" type="submit">
               <FontAwesomeIcon icon="edit" size="1x" />                            
             </button>
-            {/* <Button className='universal-button'variant="primary" type="submit">
-              <FontAwesomeIcon icon="edit" size="1x" />                            
-            </Button> */}
           </Link>
         </div>
       )
@@ -93,23 +98,107 @@ class Post extends Component {
       return null;
     }
   }
- 
+
+  getForecast = () => {
+    const { long, lat } = this.state;
+    const params = 'waveHeight,wavePeriod,waterTemperature,windSpeed';
+    ForecastService.getForecast(lat,long,params)
+      .then((response) => {
+        this.setState({
+          forecastData: response.hours,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  
+  toggleImageForecast = () => {
+    const { forecastToggle } = this.state;
+    if(forecastToggle === true) {
+      this.setState({forecastToggle: false})
+    } else if (forecastToggle === false) {
+      this.getForecast();
+      this.setState({forecastToggle: true})  
+    }
+  }
+
+  renderForecast = (imageUrl) => {
+    const { forecastToggle, forecastData, loading } = this.state;
+    if(forecastToggle && !loading) {
+      var date = new Date();
+      date.setDate(date.getDate() + 2);
+      return (
+        <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Now</th>
+            <th>Tommorow</th>
+            <th>{date.toDateString()}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Height</th>
+            <td>{forecastData[0].waveHeight[0].value}m</td>
+            <td>{forecastData[24].waveHeight[0].value}m</td>            
+            <td>{forecastData[48].waveHeight[0].value}m</td>
+          </tr>
+          <tr>
+            <th>Period</th>
+            <td>{forecastData[0].wavePeriod[0].value}s</td>
+            <td>{forecastData[24].wavePeriod[0].value}s</td>            
+            <td>{forecastData[48].wavePeriod[0].value}s</td>
+          </tr>
+          <tr>
+            <th>Sea Temp.</th>
+            <td>{forecastData[0].waterTemperature[0].value}°C</td>
+            <td>{forecastData[24].waterTemperature[0].value}°C</td>            
+            <td>{forecastData[48].waterTemperature[0].value}°C</td>
+          </tr>
+          <tr>
+            <th>Wind</th>            
+            <td>{forecastData[0].windSpeed[0].value}mps</td>
+            <td>{forecastData[24].windSpeed[0].value}mps</td>            
+            <td>{forecastData[48].windSpeed[0].value}mps</td>
+          </tr>
+        </tbody>
+      </Table>
+      )
+    } else if (!forecastToggle) {
+      return (
+          <div id='post-container'>
+            <img id='post-image' src={imageUrl} alt='selected-post'/>
+          </div>
+      )
+    } else if (forecastToggle && loading) {
+      return (
+        <div id='spinner-center'>
+          <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+      )
+    }
+  }
+
   render() {
     const { username, createdAt, description, imageUrl, location, profileImg} = this.state;  
     return (
       <div id='post'>
-        
        <div>
          <div className='top-post'>
           <Link className='username-post' to={`/profile/${this.state.creatorId}`}>
             <img src={profileImg} alt='profile-img'/>
             <h3>{username}</h3>
           </Link>
-          {this.userAccess()}
+          <button className="universal-button" onClick={this.toggleImageForecast}>
+            <FontAwesomeIcon icon="water" size="1x" /> <strong>Forecast</strong>                                     
+          </button>
+          {this.userAccess(imageUrl)}
          </div>
-
         <h5 id='location'>{location}</h5>
-        <img src={imageUrl} alt='selected-post'/>
+        {this.renderForecast(imageUrl)}
         <div className='bottom-post'>
           <Like userId={this.props.user._id} paramsId={this.props.match.params.id} />
           <p id='date'>{new Date(createdAt).toDateString()}</p>
