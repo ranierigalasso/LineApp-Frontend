11acd52a35e2992ca69ef75fd1309af2437ed0c1
ranierigalasso <ranierigalasso@gmail.com>
2019-03-06 14:13:39 +0100

    mapbox final fix

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,54 +4,41 @@ import mapboxgl from 'mapbox-gl';
 import '../stylesheets/Map.css'
 
 export default class Map extends Component {
+  map = null
+  markers = []
+  state = {
+    map: null
+  }
 
   componentDidMount = () => {
     this.renderMap();
+    this.createMarkers();
   }
   
   renderMap = () => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 0,
+      zoom: 1,
+      center: [2.173404,41.385063],
     });
-    console.log(map.style)
-    const {posts} = this.props;
-    map.loadImage('https://i.imgur.com/MK4NUzI.png', (error, image) => {
-      if (error) throw error;
-      map.addImage('custom-marker', image);
-      posts.map((post,index) => {
-        /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
-        map.addLayer({
-          id: `markers${index}`,
-          type: 'symbol',
-          /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
-          source: {
-            type: 'geojson',
-            data: {
-              type: 'FeatureCollection',
-              features: [
-                {
-                  type: 'Feature',
-                  properties: {"title": `${post.location}`},
-                  geometry: {
-                    type: 'Point',
-                    coordinates: [post.coords.coordinates[0], post.coords.coordinates[1]],
-                  },
-                },
-              ],
-            },
-          },
-          layout: {
-            'icon-image': 'custom-marker',
-          },
-        });
-      });
-    })
+  }
+
+  createMarkers = () => {
+    const { posts } = this.props;    
+    posts.forEach(post => {
+      const popup = new mapboxgl.Popup({ offset: 25 })
+        .setText(post.location);
+      const poi = new mapboxgl.Marker()
+        .setLngLat([post.coords.coordinates[0], post.coords.coordinates[1]])
+        .setPopup(popup)
+        .addTo(this.map);
+      this.markers.push(poi);
+    });
   }
   
-  render() {
+  render() {    
     return (
       <div className='map-container'>
         <div className="container" id="map" ></div>                
